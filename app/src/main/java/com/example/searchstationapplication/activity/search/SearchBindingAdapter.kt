a6f6958fac419aaae80ec07bf3b502aff5bf8302
gjgustjd <gjgustjd70@naver.com>
2022-05-03 05:44:54 +0900

    :sparkles:Feat:검색 기능 구현
    검색창을 관찰하여 텍스트가 입력될 때마다 RecyclerAdapter에 일치하는 데이터 새로 입력

--- app/src/main/java/com/example/searchstationapplication/activity/search/SearchBindingAdapter.kt
+++ app/src/main/java/com/example/searchstationapplication/activity/search/SearchBindingAdapter.kt
@@ -1,5 +1,8 @@
 package com.example.searchstationapplication.activity.search
 
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.searchstationapplication.model.dto.SubWayStation
@@ -12,5 +15,24 @@ object SearchBindingAdapter {
         stationList?.let {
             view.adapter = RecyclerStationsListAdapter(stationList)
         }
+    }
+
+    @BindingAdapter("BindViewModel")
+    @JvmStatic
+    fun watchEditText(editText: EditText,viewModel: SearchViewModel)
+    {
+        editText.addTextChangedListener(object :TextWatcher
+        {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                viewModel.performSearch(s.toString())
+            }
+
+        })
     }
 }
