bc366cc1fcd793973d02fc8276f17ae18b806414
gjgustjd <gjgustjd70@naver.com>
2022-05-05 04:57:36 +0900

    :recycle:Refactor:SearchViewModel 변수 수정
    searchDataResponse를 일반 변수 stationList로 수정
    searchedStationList를 MutableLiveData로 변경

--- app/src/main/java/com/example/searchstationapplication/activity/search/SearchViewModel.kt
+++ app/src/main/java/com/example/searchstationapplication/activity/search/SearchViewModel.kt
@@ -1,34 +1,33 @@
 package com.example.searchstationapplication.activity.search
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.searchstationapplication.model.MainRepository
 import com.example.searchstationapplication.model.dto.SubWayStation
-import com.example.searchstationapplication.model.dto.SubWayStationDataResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(private val repository: MainRepository) : ViewModel() {
-    val searchDataResponse = MutableStateFlow<SubWayStationDataResponse?>(null)
-    val searchedStationList = MutableStateFlow<List<SubWayStation>?>(null)
+    private lateinit var stationList: List<SubWayStation>
+    val searchedStationList = MutableLiveData<List<SubWayStation>>()
 
     fun setupSearchedData() {
         viewModelScope.launch {
             val response = repository.getSubwayStationData()
             if (response.isSuccessful) {
-                searchDataResponse.value = response.body()
-                searchedStationList.value = searchDataResponse.value!!.subway_stations
+                stationList = response.body()!!.subway_stations
+                searchedStationList.value = stationList
             }
         }
     }
 
     fun performSearch(text: String) {
         searchedStationList.value =
-            searchDataResponse.value!!.subway_stations.filter { it.name.contains(text) }
+            stationList.filter { it.name.contains(text) }
     }
 
     fun saveStation(item: SubWayStation) {
