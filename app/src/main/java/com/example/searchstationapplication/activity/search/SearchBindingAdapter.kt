1424f7892ecdd66924c4e1819abc4277eb1c2951
gjgustjd <gjgustjd70@naver.com>
2022-05-05 20:38:21 +0900

    :art:Refactor:지하철 역 데이터 요청 시 예외 처리
    지하철 역 데이터 요청 시 응답에 따라 다르게 동작하도록 처리
    응답 형식 정의한 ApiResponse 클래스 추가

--- app/src/main/java/com/example/searchstationapplication/activity/search/SearchBindingAdapter.kt
+++ app/src/main/java/com/example/searchstationapplication/activity/search/SearchBindingAdapter.kt
@@ -4,11 +4,15 @@ import android.app.Activity
 import android.content.Intent
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.searchstationapplication.model.dto.ApiResponse.*
 import com.example.searchstationapplication.activity.main.MainActivity
+import com.example.searchstationapplication.model.dto.ApiResponse
 import com.example.searchstationapplication.model.dto.SubWayStation
 
 object SearchBindingAdapter {
@@ -17,16 +21,30 @@ object SearchBindingAdapter {
     @JvmStatic
     fun setupRecyclerSearch(
         view: RecyclerView,
-        stationList: List<SubWayStation>?,
+        stationList: ApiResponse<List<SubWayStation>>?,
         viewModel: SearchViewModel?,
         activity: SearchActivity?
     ) {
+        fun onFail(message: String, source: String) {
+            Log.e("setupRecyclerSearch", source)
+            Toast.makeText(activity, message, Toast.LENGTH_SHORT).show()
+            activity!!.goBack()
+        }
+
         if (stationList == null)
             activity!!.showLoadingDialog()
         else {
             if (viewModel != null) {
                 activity!!.dismissLoadingDialog()
-                view.adapter = RecyclerSearchStationsListAdapter(stationList!!, viewModel)
+                when (stationList) {
+                    is Success ->
+                        view.adapter =
+                            RecyclerSearchStationsListAdapter(stationList.data, viewModel)
+                    is Failure ->
+                        onFail("데이터 수신에 실패했습니다.", stationList.message)
+                    is Exception ->
+                        onFail("오류가 발생했습니다.", stationList.throwable.stackTraceToString())
+                }
             }
         }
     }
--- app/src/main/java/com/example/searchstationapplication/activity/search/SearchViewModel.kt
+++ app/src/main/java/com/example/searchstationapplication/activity/search/SearchViewModel.kt
@@ -5,16 +5,19 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.searchstationapplication.model.MainRepository
+import com.example.searchstationapplication.model.dto.ApiResponse
+import com.example.searchstationapplication.model.dto.ApiResponse.*
 import com.example.searchstationapplication.model.dto.SubWayStation
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.Exception
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(private val repository: MainRepository) : ViewModel() {
-    var allStationList: List<SubWayStation>? = null
-    val searchedStationList = MutableLiveData<List<SubWayStation>>(null)
+    private var allStationList: List<SubWayStation>? = null
+    val searchedStationList = MutableLiveData<ApiResponse<List<SubWayStation>>>(null)
 
     init {
         setupSearchedData()
@@ -23,21 +26,26 @@ class SearchViewModel @Inject constructor(private val repository: MainRepository
     private fun setupSearchedData() {
         viewModelScope.launch {
             val response = repository.getSubwayStationData()
-            if (response.isSuccessful) {
-                allStationList = response.body()!!.subway_stations
-                searchedStationList.value = listOf()
-            } else {
-                Log.e("setupSearchedData", response.errorBody()!!.source().toString())
-            }
+            searchedStationList.value =
+                try {
+                    if (response.isSuccessful) {
+                        allStationList = response.body()!!.subway_stations
+                        Success(listOf())
+                    } else {
+                        Failure(response.errorBody()!!.source().toString())
+                    }
+                } catch (e: Exception) {
+                    ApiResponse.Exception(e)
+                }
         }
     }
 
     fun performSearch(text: String) {
-        searchedStationList.value =
+        searchedStationList.value = Success(
             if (text.isNullOrBlank())
                 listOf()
             else
-                allStationList!!.filter { it.name.contains(text) }
+                allStationList!!.filter { it.name.contains(text) })
     }
 
     fun saveStation(item: SubWayStation) {
